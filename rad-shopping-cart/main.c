c5c46bbc290beab570caf51ce3d8a160bf40d14c
Divam Kumar <divamk.business@gmail.com>
2022-11-14 04:50:09 -0800

    first commit

---
+++ rad-shopping-cart/main.c
@@ -0,0 +1,23 @@
+/*
+ * rad-shopping-cart.c
+ *
+ * Authors : Divam Kumar, Rajath Rao, Anthony Flores
+ */ 
+
+#include <avr/io.h>
+
+void setup()
+{}
+	
+void tick()
+{}
+	
+int main(void)
+{
+	setup();
+    while (1) 
+    {
+		tick();
+    }
+}
+
