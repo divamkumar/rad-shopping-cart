c52739f719583c9dff7720d90f745c3830a24c8b
Divam Kumar <divamk.business@gmail.com>
2022-11-14 21:14:45 -0800

    test code for wifi connection

--- rad-shopping-cart/main.c
+++ rad-shopping-cart/main.c
@@ -5,18 +5,30 @@
  */ 
 
 #include <avr/io.h>
+#include <SPI.h>
+#include <WiFi.h>
 
+//SSID of your network
+char ssid[] = "radtestnet";
+//password of your WPA Network
+char pass[] = "radshoppingcart";
 
-void tick()
+void setup()
 {
+ WiFi.begin(ssid, pass);
+
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Couldn't get a wifi connection");
+    while(true);
+  }
+  // if you are connected, print out info about the connection:
+  else {
+   // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("RSSI:");
+  Serial.println(rssi);
+  }
 }
 
-	
-int main(void)
-{
-    while (1) 
-    {
-		tick();
-    }
-}
+void loop () {}
 
